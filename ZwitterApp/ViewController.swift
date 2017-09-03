f38d32b64c7556813f4ab19680a487aa07a8f1eb
Muhammed Esad Kaya <mesadkaya@Muhammed-MacBook-Pro.local>
2017-09-03 08:43:53 +0300

    Login ve Register sayfalarının tasarımları bitti.

--- ZwitterApp/ViewController.swift
+++ ZwitterApp/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signupButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Button corner radius (butona ovallik verme)
+        signupButton.layer.cornerRadius = 8
+        
     }
 
     override func didReceiveMemoryWarning() {
